d66c80070add2e877d9da51f0356f4b353137670
poligami.my@gmail.com <78262483+dinokv@users.noreply.github.com>
2021-03-28 19:10:25 +0800

    Adding Fields to View

---
+++ public/index.js
@@ -0,0 +1,30 @@
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      'total_amount' : 1000
+    }
+  }
+  render(){
+    return(
+      <div>
+        <h1> BLOCKCHAIN APPLICATION ONZ WEB 2.0 </h1>
+        <div>
+          <p> Total Coin amount is {this.state.total_amount}</p>
+        </div>
+        <form>
+          <input placeholder="amount" />
+          <input placeholder="email" />
+          <button> Participate </button>
+        </form>
+      </div>
+    )
+  }
+};
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>
+  , document.getElementById('reactBinding')
+);
